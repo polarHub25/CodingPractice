5614a4dbff6a525f4a4cd067ea49416e0f20f2c5
polarHub25 <olivetree258@gmail.com>
2022-07-09 20:45:29 +0900

    프로그래머스 - lv1 - 평균구하기

---
+++ programmers/src/main/java/org/hm/lv1/Average.java
@@ -0,0 +1,15 @@
+package org.hm.lv1;
+
+public class Average {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        for(int i=0; i< arr.length; i++){
+            answer += arr[i];
+        }
+        answer = answer/arr.length;
+
+        return answer;
+    }
+
+}
