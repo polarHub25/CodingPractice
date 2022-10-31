e68fa38ea9bb97b2e7a3886119e2b1761b4cd0f3
polarHub25 <olivetree258@gmail.com>
2022-10-31 16:56:09 +0900

    프로그래머스 - 문자열 다루기 기본

--- programmers/src/main/java/org/hm/lv1/Main.java
+++ programmers/src/main/java/org/hm/lv1/Main.java
@@ -9,86 +9,8 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args)
     {
-//        TakingRectangularStar takingRectangularStar = new TakingRectangularStar();
-//        takingRectangularStar.solution(args);
-
-//        RandomIntervalRandomNumber randoIntervalRandomNumber = new RandomIntervalRandomNumber();
-//       long[] answer =  randoIntervalRandomNumber.solution(2,5);
-
-//        String answer = solution("01044455555");
-//        System.out.println(answer);
-//        Boolean asnswer = solution(161);
-//        System.out.println(asnswer);
-
-        //int answer = solution(626331);
-
-//        int[] param = {4,3,2,10};
-//        //int[] param = {10};
-//        int[] answer = solution(param);
-
-       // GcdLcmSolution sol = new GcdLcmSolution();
-        //int[] answer = solution(135,25);
-//        int[] answer = solution(18,45);
-//        System.out.println(answer[0] + " , "+ answer[1]);
-/*
-        SumOfDivisor sol = new SumOfDivisor();
-        int answer = sol.solution(12);
-
-        System.out.println(answer);*/
-
-/*        AddDigit sol = new AddDigit();
-        sol.solution(234);*/
-
-//        DigitFlip sol = new DigitFlip();
-//        sol.solution(12345);
-
-//        CountOfString sol = new CountOfString();
-//        sol.solution("pPOOOOyY");
-
-//        DescOrder sol = new DescOrder();
-//        sol.solution(118372);
-
-//        ReplaceStrToInt sol = new ReplaceStrToInt();
-//        int answer = sol.solution("1234");
-//
-//        int answer2 = sol.solution("-1234");
-
-//        FindRemainder sol = new FindRemainder();
-//        sol.solution(10);
-
-//        BetweenSum sol = new BetweenSum();
-//        sol.solution(3,5);
-
-//         int[] param = {5,9,7,10};
-//        int[] param2 = {3,2,6};
-//         int[] answer = {};
-//         DivisorZero sol = new DivisorZero();
-//         answer = sol.solution(param, 5);
-//
-//        sol.solution(param2, 10);
-
-//        NoneNumAdd sol = new NoneNumAdd();
-//        int[] param = {1,2,3,4,6,7,8,0};
-//        int answer = sol.solution(param);
-
-//        GetCenterStr sol = new GetCenterStr();
-//        sol.solution("abcdef");
-//        //sol.solution("qwer");
-
-//        PatternReturn sol = new PatternReturn();
-//        String answer = sol.solution(6);
-//        System.out.println("test======================="+answer);
-//
-//        DotProduct sol = new DotProduct();
-//        int[] a = {1,2,3,4};
-//        int[] b = {-3,-1,0,2};
-//
-//        sol.solution(a,b);
-
-        StringOrder sol = new StringOrder();
-        sol.solution("Zbcdefg");
-
-
+        AssortStr sol = new AssortStr();
+        sol.solution("a1234");
 
     }
 
