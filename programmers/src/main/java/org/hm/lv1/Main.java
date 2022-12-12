468763fa9758a6a9d6c2ad960310301bfa258272
polarHub25 <olivetree258@gmail.com>
2022-12-12 21:52:33 +0900

    코딩 테스트 -  완주하지못한 선수 ( 해시 사용)

--- programmers/src/main/java/org/hm/lv1/Main.java
+++ programmers/src/main/java/org/hm/lv1/Main.java
@@ -4,14 +4,15 @@ package org.hm.lv1;
 public class Main {
     public static void main(String[] args)
     {
-        int[][] queries = {{1,4},{2,2},{1,3},{1,6},{3,-1},{2,-1}};
-        StackBottom sol = new StackBottom();
-        int[] answer = sol.solution(3, queries);
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        UnfinishedPlayer sol = new UnfinishedPlayer();
+        String answer = sol.solution(participant,completion);
 
-        if(answer[0]==4){
+        if(answer=="leo"){
             System.out.println("pass");
         }else{
-            System.out.println("fail========"+answer[0]);
+            System.out.println("fail========"+answer);
         }
 
 
