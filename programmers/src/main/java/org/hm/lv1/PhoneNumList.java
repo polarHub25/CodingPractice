4a26341b5b15d04ad0b9839dbc392d5819a7c549
polarHub25 <olivetree258@gmail.com>
2022-12-17 20:31:13 +0900

    코딩 테스트 -  전화번호 목록 겹치는 부분 찾기 문제(l해시로 풀이)

--- programmers/src/main/java/org/hm/lv1/PhoneNumList.java
+++ programmers/src/main/java/org/hm/lv1/PhoneNumList.java
@@ -1,6 +1,8 @@
 package org.hm.lv1;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 * 문제 설명
@@ -25,11 +27,25 @@ public class PhoneNumList {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
 
-        Arrays.sort(phone_book);
+//        Arrays.sort(phone_book);
+//
+//        for(int i = 0; i < phone_book.length-1; i++){
+//            if(phone_book[i+1].startsWith(phone_book[i])){
+//                return false;
+//            }
+//        }
 
-        for(int i = 0; i < phone_book.length-1; i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                return false;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i< phone_book.length; i++){
+            map.put(phone_book[i], i);
+        }
+
+        for(int j =0; j< phone_book.length;j++){
+            for(int z=0; z< phone_book[j].length();z++){
+                if(map.containsKey(phone_book[j].substring(0,z))){
+                    return false;
+                }
             }
         }
 
